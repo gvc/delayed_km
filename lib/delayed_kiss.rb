b449fc1b0873ac6609e93a2c85a45bbfa94e72b1
Guilherme Carvalho <guilherme@guava.com.br>
2012-12-01 10:21:48 -0300

    Initial refactor, adding a Kissmetric class

--- lib/delayed_kiss.rb
+++ lib/delayed_kiss.rb
@@ -4,6 +4,8 @@ require 'delayed_kiss/railtie' if defined?(Rails)
 require 'httparty'
 
 module DelayedKiss
+  class ConfigurationError < StandardError; end
+
   mattr_accessor :key
   @@key = nil
   
@@ -18,48 +20,78 @@ module DelayedKiss
   end
 
   def self.record(id, event, query_params={})
-    self.verify_configuration
-    raise ArgumentError.new("id can't be blank") if id.blank?
-    raise ArgumentError.new("event can't be blank") if event.blank?
-    
-    query_params.merge!({
-      '_n' => event,
-      '_p' => id,
-      '_t' => Time.now.to_i.to_s,
-      '_k' => @@key
-    })
-    HTTParty.delay.get('https://trk.kissmetrics.com/e?' + query_params.to_param) unless @@key.blank?
+    Kissmetric.new(@@key, @@whiny_config, @@config_file).
+      record(id, event, query_params)
   end
 
   def self.alias(alias_from, alias_to)
-    self.verify_configuration
-    raise ArgumentError.new("you must specify both a from a to value") if alias_from.blank? || alias_to.blank?
-    
-    query_params = {
-      '_n' => alias_to,
-      '_p' => alias_from,
-      '_t' => Time.now.to_i.to_s,
-      '_k' => @@key
-    }
-    HTTParty.delay.get('https://trk.kissmetrics.com/a?' + query_params.to_param) unless @@key.blank?
+    Kissmetric.new(@@key, @@whiny_config, @@config_file).
+      alias(alias_from, alias_to)
   end
 
   def self.set(id, query_params)
-    self.verify_configuration
-    raise ArgumentError.new("id can't be blank") if !id || id.blank?
-    return if query_params.blank? # don't do anything if we're not setting any values on the identity
-    
-    query_params.merge!({
-      '_p' => id,
-      '_t' => Time.now.to_i.to_s,
-      '_k' => @@key
-    })
-    HTTParty.delay.get('https://trk.kissmetrics.com/s?' + query_params.to_param) unless @@key.blank?
+    Kissmetric.new(@@key, @@whiny_config, @@config_file).
+      set(id, query_params)
   end
-  
-  def self.verify_configuration
-    raise DelayedKiss::ConfigurationError if @@whiny_config && @@key.blank?
+
+  class Kissmetric
+    attr_accessor :key, :whiny_config, :config_file
+
+    include HTTParty
+    base_uri "https://trk.kissmetrics.com"
+
+    def initialize(key, whiny_config, config_file)
+      @key = key
+      @whiny_config = whiny_config
+      @config_file = config_file
+
+      raise DelayedKiss::ConfigurationError if @whiny_config && @key.blank?
+    end
+
+    def alias(alias_from, alias_to)
+      raise ArgumentError.new("you must specify both a from a to value") if alias_from.blank? || alias_to.blank?
+      
+      query_params = {
+        '_n' => alias_to,
+        '_p' => alias_from,
+        '_t' => Time.now.to_i.to_s,
+        '_k' => @key
+      }
+
+      self.delay.request_for_api('/a?' + query_params.to_param) unless @key.blank?
+    end
+
+    def record(id, event, query_params={})
+      raise ArgumentError.new("id can't be blank") if id.blank?
+      raise ArgumentError.new("event can't be blank") if event.blank?
+      
+      query_params.merge!({
+        '_n' => event,
+        '_p' => id,
+        '_t' => Time.now.to_i.to_s,
+        '_k' => @key
+      })
+
+      self.delay.request_for_api('/e?' + query_params.to_param) unless @key.blank?
+    end
+
+    def set(id, query_params)
+      raise ArgumentError.new("id can't be blank") if !id || id.blank?
+      return if query_params.blank? # don't do anything if we're not setting any values on the identity
+      
+      query_params.merge!({
+        '_p' => id,
+        '_t' => Time.now.to_i.to_s,
+        '_k' => @key
+      })
+
+      self.delay.request_for_api('/s?' + query_params.to_param) unless @key.blank?
+    end
+
+  protected
+
+    def request_for_api(request)
+      self.class.get(request)
+    end
   end
-  
-  class ConfigurationError < StandardError; end
 end
--- spec/delayed_kiss_spec.rb
+++ spec/delayed_kiss_spec.rb
@@ -22,12 +22,12 @@ describe DelayedKiss do
   
   describe :record do
     it "should send a request to the KISSmetrics API when valid parameters are supplied" do
-     HTTParty.expects(:get).once.returns(true)
+     DelayedKiss::Kissmetric.expects(:get).once.returns(true)
      DelayedKiss.record("identity", "event")
     end
     
     it "should raise an error without an identity" do
-      HTTParty.expects(:get).never
+      DelayedKiss::Kissmetric.expects(:get).never
       expect { DelayedKiss.record(nil, "event") }.to raise_error(ArgumentError)
     end
     
@@ -36,7 +36,7 @@ describe DelayedKiss do
     end
     
     it "should send a request to the KISSmetrics API with custom properties in the URL's query string" do
-      HTTParty.expects(:get).with(regexp_matches(/type=crazy/)).returns(true)
+      DelayedKiss::Kissmetric.expects(:get).with(regexp_matches(/type=crazy/)).returns(true)
       DelayedKiss.record("identity", "event", {:type => "crazy"})
     end
     
@@ -62,17 +62,17 @@ describe DelayedKiss do
   
   describe :alias do
     it "should send a request to the KISSmetrics API when valid parameters are supplied" do
-      HTTParty.expects(:get).once.returns(true)
+      DelayedKiss::Kissmetric.expects(:get).once.returns(true)
       DelayedKiss.alias("oldname", "newname")
     end
     
     it "should include the from alias in the query string" do
-      HTTParty.expects(:get).with(regexp_matches(/_p=oldname/)).returns(true)
+      DelayedKiss::Kissmetric.expects(:get).with(regexp_matches(/_p=oldname/)).returns(true)
       DelayedKiss.alias("oldname", "newname")
     end
     
     it "should include the to alias in the query string" do
-      HTTParty.expects(:get).with(regexp_matches(/_n=newname/)).returns(true)
+      DelayedKiss::Kissmetric.expects(:get).with(regexp_matches(/_n=newname/)).returns(true)
       DelayedKiss.alias("oldname", "newname")
     end
     
@@ -96,7 +96,7 @@ describe DelayedKiss do
     end
     
     it "should not send a request to the KISSmetrics API if the API key is blank and the whiny config is turned off" do
-      HTTParty.expects(:get).never
+      DelayedKiss::Kissmetric.expects(:get).never
       
       DelayedKiss.key = nil
       DelayedKiss.whiny_config = false
@@ -106,17 +106,17 @@ describe DelayedKiss do
   
   describe :set do
     it "should send a request to the KISSmetrics API when valid parameters are supplied" do
-      HTTParty.expects(:get).once.returns(true)
+      DelayedKiss::Kissmetric.expects(:get).once.returns(true)
       DelayedKiss.set("identity", {:gender => :male})
     end
     
     it "should include the set properties in the query string" do
-      HTTParty.expects(:get).with(regexp_matches(/age=27/)).returns(true)
+      DelayedKiss::Kissmetric.expects(:get).with(regexp_matches(/age=27/)).returns(true)
       DelayedKiss.set("identity", {:age => 27})
     end
     
     it "should not send a reuqest to the KISSmetrics API when the set paramters are blank" do
-      HTTParty.expects(:get).never
+      DelayedKiss::Kissmetric.expects(:get).never
       DelayedKiss.set("identity", {})
     end
     
@@ -136,7 +136,7 @@ describe DelayedKiss do
     end
     
     it "should not send a request to the KISSmetrics API if the API key is blank and the whiny config is turned off" do
-      HTTParty.expects(:get).never
+      DelayedKiss::Kissmetric.expects(:get).never
       
       DelayedKiss.key = nil
       DelayedKiss.whiny_config = false
